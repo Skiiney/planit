021f91e69d461724cf02711a7069487197f31309
skiiney <skiiney@8chan.co>
2018-10-19 22:52:56 -0300

    Bom di

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 import 'routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+
+final FirebaseApp app = FirebaseApp(
+  name: 
+  options: FirebaseOptions(
+    googleAppID:'',
+    apiKey: '',
+    databaseURL: ''
+  )
+);
 
 void main() => runApp(MyApp());
 
